9930e4521d8425a9e84dab17ef4c70d45b391e98
easydaniel <whitetiger50512@gmail.com>
2016-02-24 10:49:36 +0800

    rwd navbar done

--- easylin.js
+++ easylin.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+	// Initialize
 	createNavbarMenuAnimation();
+	createVerdictTypeSwitchAnimation();
 });
 
 function createNavbarMenuAnimation() {
@@ -9,3 +11,28 @@ function createNavbarMenuAnimation() {
 		$(this).toggleClass('open');
 	});
 }
+
+function createVerdictTypeSwitchAnimation() {
+	$('.type-switch-toggle').click(function(e) {
+		e.preventDefault();
+		$('.type-switch-item-list').slideToggle();
+	});
+	$('.type-switch-item').click(function(e) {
+		e.preventDefault();
+		$('.type-switch-toggle').html($(this).text());
+		$('.type-switch-item-list').slideToggle();
+	});
+
+	$('.type-button-switch-toggle').click(function(e) {
+		e.preventDefault();
+		$('.type-button-switch-item-list').slideToggle();
+	});
+	$('.type-button-switch-item').click(function(e) {
+		e.preventDefault();
+		$('.type-button-switch-toggle').html($(this).text());
+		$('.type-button-switch-item-list').slideToggle();
+	});
+
+
+}
+
