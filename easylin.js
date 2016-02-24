d36f4b34782d2142b239eda344a98265a4ec0de9
easydaniel <whitetiger50512@gmail.com>
2016-02-24 13:48:04 +0800

    footer, mail-link, workshop-link added

--- easylin.js
+++ easylin.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 	// Initialize
 	createNavbarMenuAnimation();
 	createVerdictTypeSwitchAnimation();
+	detectScrollToBottomEvent();
 });
 
 function createNavbarMenuAnimation() {
@@ -13,26 +14,33 @@ function createNavbarMenuAnimation() {
 }
 
 function createVerdictTypeSwitchAnimation() {
-	$('.type-switch-toggle').click(function(e) {
+	$('.type-switch-toggle').click(function (e) {
 		e.preventDefault();
 		$('.type-switch-item-list').slideToggle();
 	});
-	$('.type-switch-item').click(function(e) {
+	$('.type-switch-item').click(function (e) {
 		e.preventDefault();
 		$('.type-switch-toggle').html($(this).text());
 		$('.type-switch-item-list').slideToggle();
 	});
 
-	$('.type-button-switch-toggle').click(function(e) {
+	$('.type-button-switch-toggle').click(function (e) {
 		e.preventDefault();
 		$('.type-button-switch-item-list').slideToggle();
 	});
-	$('.type-button-switch-item').click(function(e) {
+	$('.type-button-switch-item').click(function (e) {
 		e.preventDefault();
 		$('.type-button-switch-toggle').html($(this).text());
 		$('.type-button-switch-item-list').slideToggle();
 	});
-
-
 }
 
+function detectScrollToBottomEvent() {
+	$(window).scroll(function () {
+		if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+			$('footer').addClass('show');
+		} else {
+			$('footer').removeClass('show');
+		}
+	});
+}
